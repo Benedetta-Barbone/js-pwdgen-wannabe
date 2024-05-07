bd9d5a8d4837fc8343b244ac1d2ca3716919bf43
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-07 17:26:59 +0200

    esercizio completo

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const nome = prompt('Inserisci il tuo nome');
+const cognome = prompt('Inserisci il tuo cognome');
+const colorePreferito = prompt('Inserisci il tuo colore preferito');
+
+
+console.log(nome + cognome + colorePreferito)
